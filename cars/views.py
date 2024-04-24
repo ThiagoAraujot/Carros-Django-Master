b53dca7d61b3540cf07d89dcb88e22f39b401d2b
thiago araujo <devthiagoaraujo@gmail.com>
2024-04-23 22:37:17 -0300

    feat Car's url, view and template

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def car_view(request):
+    return render(
+        request,
+        'cars.html',
+        {'cars': {'model': 'Porsche GT3 RS'}}
+    )
