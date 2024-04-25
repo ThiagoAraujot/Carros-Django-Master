388c7919d8ce4cee17dfa464bdea665f8c48b8f3
thiago araujo <devthiagoaraujo@gmail.com>
2024-04-25 15:55:41 -0300

    feat: search feature; cars.html and base.html

--- cars/views.py
+++ cars/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from cars.models import Car
 
 # Create your views here.
 
 
 def car_view(request):
+    cars = Car.objects.all().order_by('-id')
+    search = request.GET.get('search')
+    if search:
+        cars = Car.objects.filter(model__icontains=search)
+
     return render(
         request,
         'cars.html',
-        {'cars': {'model': 'Porsche GT3 RS'}}
+        {'cars': cars}
     )
