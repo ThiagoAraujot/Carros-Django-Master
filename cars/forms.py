44118d1d4f054981ffc60e1ee808eb91374485a8
thiago araujo <devthiagoaraujo@gmail.com>
2024-04-26 16:00:07 -0300

    feat: new car html and integration between Models and Forms

--- cars/forms.py
+++ cars/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from cars.models import Brand
+from cars.models import Brand, Car
 
 
 class CarForm(forms.Form):
@@ -9,3 +9,15 @@ class CarForm(forms.Form):
     model_year = forms.IntegerField()
     value = forms.FloatField()
     photo = forms.ImageField()
+
+    def save(self):
+        car = Car(
+            model=self.cleaned_data['model'],
+            brand=self.cleaned_data['brand'],
+            factory_year=self.cleaned_data['factory_year'],
+            model_year=self.cleaned_data['model_year'],
+            value=self.cleaned_data['value'],
+            photo=self.cleaned_data['photo'],
+        )
+        car.save()
+        return car
