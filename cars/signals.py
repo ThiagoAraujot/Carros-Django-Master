e0ee6d852d72ba624b4031584d887e1bde697d63
thiago araujo <devthiagoaraujo@gmail.com>
2024-05-15 11:26:50 -0300

    feat: pre_save signal to create description

--- cars/signals.py
+++ cars/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_save
 from django.db.models import Sum
 from django.dispatch import receiver
 from .models import Car, CarInventory
@@ -15,6 +15,10 @@ def car_inventory_update():
         cars_value=cars_value
     )
 
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    if not instance.bio:
+        instance.bio = "Descrição gereada automaticamente"
 
 @receiver(post_save, sender=Car)
 def car_pre_delete(sender, instance, **kwargs):
